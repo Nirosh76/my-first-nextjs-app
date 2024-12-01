778f30ab293158e6fb7b322dcf19c770c43a024a
Nirosh76 <niroshlk@gmail.com>
2024-12-01 18:39:47 +0530

    home work

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,3 +1,8 @@
 export default function Loginpage() {
-  return <div className="w-full">Login Page</div>;
+  return (
+    <div className="flex flex-col justify-center items-center w-full min-h-screen bg-yellow-200  text-black  text-center">
+      <h2 className="pb-20 font-bold2">Welcome to Next.js</h2>
+      <div className="bg-green-400 px-5 py-3 text-base rounded-lg">Login</div>
+    </div>
+  );
 }
